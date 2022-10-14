12cd9ca89c7d329ab34ab5c7124e2518b318d36a
chduong <chduong@debian.42.fr>
2022-10-14 09:20:26 +0200

    push

--- test/mariadb/config/script.sh
+++ test/mariadb/config/script.sh
@@ -43,12 +43,14 @@ else
     mariadb-install-db ${MYSQL_INSTALL_OPT}
     # initialize mariadb 'offline' (without a running daemon)
     {
+        echo "FLUSH PRIVILEGES;"
+        echo "CREATE DATABASE wordpress;"
         # delete all root user except the one with localhost as host
         echo "DELETE FROM mysql.user WHERE User = 'root' AND Host != 'localhost';"
         # change root password
-        echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MARIADB_ROOT_PASSWORD}');"
+        echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MARIADB_ROOT_PWD}');"
         # create new user
-        echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';"
+        echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PWD}';"
         # give all permissions to the new user
         echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${WP_DB_USER}'@'%';"
         # apply modifications to the grant table (maybe not necessary)
