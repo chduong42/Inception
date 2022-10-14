0af0add9267466efad47a570a735994cd0ad8244
Chi-khang DUONG <chduong@e1r4p7.clusters.42paris.fr>
2022-10-14 10:58:19 +0200

    tovm

--- srcs/mariadb/config/script.sh
+++ srcs/mariadb/config/script.sh
@@ -24,13 +24,12 @@
 #
 MYSQL_INSTALL_OPT="
     --auth-root-authentication-method=normal
+    --user=mysql
     --basedir=/usr
     --datadir=/var/lib/mysql
     --skip-test-db
-    --user=mysql
 "
-
-set -e -x
+set -xe
 
 if [ ! -d /run/mysqld ]; then
     mkdir -p /run/mysqld
@@ -41,23 +40,18 @@ printf -- "Installing MariaDB\n"
 if [ -d /var/lib/mysql/mysql ]; then
     printf -- "MariaDB already installed, skipping\n"
 else
-    # shellcheck disable=2086
     mariadb-install-db ${MYSQL_INSTALL_OPT}
-
     # initialize mariadb 'offline' (without a running daemon)
     {
-        # initialize privileges table (disabled when running in bootstrap mode)
-        echo "FLUSH PRIVILEGES;"
-        # delete all root user except the one with localhost as host
-        echo "DELETE FROM mysql.user WHERE User = 'root' AND Host != 'localhost';"
-        # change root password
-        echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MARIADB_ROOT_PASSWORD}');"
-        # create new user
-        echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';"
-        # give all permissions to the new user
-        echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${WP_DB_USER}'@'%';"
-        # apply modifications to the grant table (maybe not necessary)
-        echo "FLUSH PRIVILEGES;"
+        # Create ${DB_NAME}
+        CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+        # Create new user wordpress
+        CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED by '${WP_PASSWORD}';
+        # Give access to all database wordpress to the user 'wordpress'
+        GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO 'wordpress'@'%' WITH GRANT OPTION;
+        # Set up root password
+        SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${ROOT_PWD}');
+        FLUSH PRIVILEGES;
     } | mariadbd --user=mysql --bootstrap
 fi
 
@@ -65,3 +59,15 @@ fi
 # ':' means 'do nothing'
 : > /etc/my.cnf
 rm -rf /etc/my.cnf.d/*
+
+# replace the current shell process with the mysql server :
+#	--bind-address=0.0.0.0
+#		allow every address to connect
+#	--disable-skip-networking
+#		tell mariadb to listen for network connections
+#	-u mysql
+#		the system user account who will run the server,
+#		mandatory when running as root
+#	-v
+#		more information logging
+exec mariadbd --bind-address=0.0.0.0 --disable-skip-networking --user mysql --verbose
