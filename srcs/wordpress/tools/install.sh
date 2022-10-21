584b6361defca849c8a20f95242a2dd8d074d458
Chi-khang DUONG <chduong@e1r5p6.clusters.42paris.fr>
2022-10-21 18:58:23 +0200

    ok

--- srcs/wordpress/tools/install.sh
+++ srcs/wordpress/tools/install.sh
@@ -1,34 +1,23 @@
 #!/bin/sh
 
-set -e -x
+wp config create \
+    --dbname="${WP_DB_NAME}" \
+    --dbuser="${MYSQL_USER}" \
+    --dbpass="${MYSQL_PASSWORD}" \
+    --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-WP_PATH='/var/www/html'
-mkdir -p "${WP_PATH}"
+wp db create
 
-if ! wp core is-installed --path="${WP_PATH}" 2> /dev/null; then
-    rm -rf "${WP_PATH:?}/*"
-
-    wp config create --allow-root \
-        --dbname="${WP_DB_NAME}" \
-        --dbuser="${MYSQL_USER}" \
-        --dbpass="${MYSQL_PASSWORD}" \
-        --dbhost=mariadb:3306 \
-        --path="${WP_PATH}" \
-        --debug
-    wp db create
-    wp core install \
-        --url="chduong.42.fr" \
-        --title="${WP_TITLE}" \
-        --admin_user="${WP_ADMIN_NAME}" \
-        --admin_password="${WP_ADMIN_PASSWORD}" \
-        --admin_email="${WP_ADMIN_EMAIL}" \
-        --skip-email
-    wp user create \
-        "${WP_USER_NAME}" \
-        "${WP_USER_EMAIL}" \
-        --user_pass="${WP_USER_PASSWORD}" \
-        --porcelain
-
-fi
+wp core install \
+    --url="chduong.42.fr" \
+    --title="${WP_TITLE}" \
+    --admin_user="${WP_ADMIN_NAME}" \
+    --admin_password="${WP_ADMIN_PASSWORD}" \
+    --admin_email="${WP_ADMIN_EMAIL}" \
+    --skip-email
+    
+wp user create "${WP_USER_NAME}" "${WP_USER_EMAIL}" \
+    --user_pass="${WP_USER_PASSWORD}" \
+    --porcelain
 
 exec /usr/sbin/php-fpm7 -R -F -y /etc/php7/php-fpm.conf
