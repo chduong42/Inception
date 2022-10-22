5841db9f4e379c7743471d465f8fbb029ef25f70
chduong <chduong@debian.42.fr>
2022-10-22 02:59:37 +0200

    Mashallah

--- srcs/mariadb/tools/install.sh
+++ srcs/mariadb/tools/install.sh
@@ -1,6 +1,9 @@
+touch tmp_mdb
+chmod 755 tmp_mdb
+
 if [ -d /var/lib/mysql/mysql ]; then
 	echo "Already set up, starting..."
-	exec /usr/bin/mysqld --user=mysql --console $@
+	exec /usr/bin/mysqld --user=mysql --skip-networking=0 --console $@
 	exit 1
 fi
 
@@ -10,23 +13,19 @@ chown -R mysql:mysql /var/lib/mysql
 
 mariadb-install-db --auth-root-authentication-method=normal --basedir=/usr --datadir=/var/lib/mysql --skip-test-db --user=mysql
 
-touch tmp_mdb
-chmod 755 tmp_mdb
-cat << break > tmp_mdb
-CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;
+cat << eof > tmp_mdb
 DELETE FROM mysql.user WHERE User = 'root';
-USE wordpress;
 FLUSH PRIVILEGES;
 CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT GRANT OPTION ON $WP_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL ON $WP_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 FLUSH PRIVILEGES;
-break
+eof
 
 /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < tmp_mdb
 
 rm -f tmp_mdb
 
-exec /usr/bin/mysqld --user=mysql --console $@
+exec /usr/bin/mysqld --user=mysql --skip-networking=0 --console $@
--- srcs/wordpress/tools/install.sh
+++ srcs/wordpress/tools/install.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-wp config create \
+#Wait mariadb launched
+sleep 6
+
+set -x
+
+cd /var/www/wordpress
+
+if [ -f /var/www/wordpress/wp-config.php ]; then
+    echo "Already created"
+    exec /usr/sbin/php-fpm7 -F
+    exit 1
+fi
+
+# wp core download --path="/var/www/wordpress"
+wp config create --allow-root \
     --dbname="${WP_DB_NAME}" \
     --dbuser="${MYSQL_USER}" \
     --dbpass="${MYSQL_PASSWORD}" \
-    --dbhost=mariadb:3306 --path='/var/www/wordpress'
-
+    --dbhost=mariadb:3306 \
+    --path='/var/www/wordpress'
 wp db create
-
 wp core install \
     --url="chduong.42.fr" \
     --title="${WP_TITLE}" \
@@ -15,9 +28,10 @@ wp core install \
     --admin_password="${WP_ADMIN_PASSWORD}" \
     --admin_email="${WP_ADMIN_EMAIL}" \
     --skip-email
-    
 wp user create "${WP_USER_NAME}" "${WP_USER_EMAIL}" \
     --user_pass="${WP_USER_PASSWORD}" \
     --porcelain
 
-exec /usr/sbin/php-fpm8 -R -F -y /etc/php7/php-fpm.conf
+mkdir -p /run/php
+
+exec /usr/sbin/php-fpm7 -F
